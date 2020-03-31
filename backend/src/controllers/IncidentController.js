ec01efb55ba162193d264f448a75d11cdb820077
Julio Cesar <juliojc12@gmail.com>
2020-03-30 21:00:19 -0300

    att func backend

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -2,24 +2,23 @@ const connection = require('../database/connection')
 
 module.exports = {
     async index(request, response) {
+        const { page = 1 } = request.query;
 
-        const { page = 1 } = request.query
-
-        const [count] = await connection('incidents').count()
+        const [count] = await connection('incidents').count();
 
         const incidents = await connection('incidents')
-            .join('ongs', 'ongs.id', '=' ,'incidents.ong_id')
-            .limit(5)
-            .offset((page - 1) * 5)
-            .select(['incidents.*',
-                 'ongs.name', 
-                 'ongs.email', 
-                 'ongs.whatsapp',
-                 'ongs.city', 
-                 'ongs.uf'
-                ])
+        .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+        .limit(5)
+        .offset((page - 1)* 5)
+        .select([
+            'incidents.*', 
+            'ongs.name', 
+            'ongs.email', 
+            'ongs.whatsapp', 
+            'ongs.city', 
+            'ongs.uf']);
 
-        response.header('X-Total-Count', count['count(*)'])
+        response.header('X-Total-Count', count['count(*)']);
 
         return response.json(incidents);
     },
@@ -38,23 +37,21 @@ module.exports = {
         return response.json({ id });
     },
 
-
     async delete(request, response) {
-        const { id } = request.params
+        const { id } = request.params;
         const ong_id = request.headers.authorization;
 
         const incident = await connection('incidents')
             .where('id', id)
             .select('ong_id')
-            .first()
+            .first();
 
-        if(incident.ong_id !== ong_id) {
-            return response.status(401).json({ error: 'Operation not Permited.' })
+        if (incident.ong_id != ong_id){
+            return response.status(401).json({ error: 'Operation not permited.' });
         }
 
-        await connection('incidents').where('id', id).delete()
+        await connection('incidents').where('id', id).delete();
 
-        return response.status(204).send()
-
+        return response.status(204).send();
     }
-}
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const cors = require('cors')
-const routes = require('./routes')
+const express = require('express');
+const cors = require('cors');
+const { errors } = require('celebrate')
+const routes = require('./routes');
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use(errors());
 
-app.use(express.json())
-app.use(routes)
-
-app.listen(3333)
+module.exports = app;
 
 /**
  * Rota /Recurso
